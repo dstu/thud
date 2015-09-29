5ebb0cbb555ec62ade748e06efb2b6e72efaba50
Stu Black <trurl@freeshell.org>
2015-09-28 21:40:12 -0400

    Action projections and resolution.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use std::iter::Iterator;
-use std::ops::Index;
 
 #[derive(Clone, Copy, Eq, PartialEq)]
 pub enum Piece {
@@ -8,13 +7,13 @@ pub enum Piece {
     Troll,
 }
 
+// For now, assume that we're on a standard Thud grid.
 #[derive(Clone, Copy, Eq, PartialEq)]
 pub struct Coordinate(u8);
 
 const ROW_LENGTHS: [u8; 15] = [5, 7, 9, 11, 13, 15, 15, 15, 15, 15, 13, 11, 9, 7, 5];
 const ROW_OFFSETS: [u8; 15] = [0, 5, 12, 21, 32, 45, 60, 75, 90, 105, 120, 133, 144, 153, 160];
 
-// For now, all numerical operations assume that we're on a standard Thud grid.
 impl Coordinate {
     pub fn new(c: u8) -> Self {
         assert!(c <= 165u8);
@@ -74,6 +73,22 @@ impl Coordinate {
                 Some(Coordinate::new(ROW_OFFSETS[(row - 1) as usize] + col))
             }
         }
+    }
+
+    pub fn up_left(self) -> Option<Self> {
+        self.up().and_then(|n| n.left())
+    }
+
+    pub fn up_right(self) -> Option<Self> {
+        self.up().and_then(|n| n.right())
+    }
+
+    pub fn down_left(self) -> Option<Self> {
+        self.down().and_then(|n| n.left())
+    }
+
+    pub fn down_right(self) -> Option<Self> {
+        self.down().and_then(|n| n.right())
     }
 
     pub fn down(self) -> Option<Self> {
@@ -136,9 +151,9 @@ pub struct BoardState {
 
 impl BoardState {
     /// Returns a freshly constructed board for a standard game of Thud.
-    pub fn init() -> Self {
+    pub fn new() -> Self {
         let mut board = BoardState { spaces: [None; 165], };
-        // Place Dwarves.
+        // Place Dwarfs.
         let mut c = Coordinate::new(0u8);
         while board.piece_at(c) != Some(Piece::Dwarf) {
             let row = c.row();
@@ -156,10 +171,10 @@ impl BoardState {
         board.set_piece_at(c.down().unwrap(), Some(Piece::Troll));
         board.set_piece_at(c.left().unwrap(), Some(Piece::Troll));
         board.set_piece_at(c.right().unwrap(), Some(Piece::Troll));
-        board.set_piece_at(c.up().and_then(|n| n.left()).unwrap(), Some(Piece::Troll));
-        board.set_piece_at(c.up().and_then(|n| n.right()).unwrap(), Some(Piece::Troll));
-        board.set_piece_at(c.down().and_then(|n| n.left()).unwrap(), Some(Piece::Troll));
-        board.set_piece_at(c.down().and_then(|n| n.right()).unwrap(), Some(Piece::Troll));
+        board.set_piece_at(c.up_left().unwrap(), Some(Piece::Troll));
+        board.set_piece_at(c.up_right().unwrap(), Some(Piece::Troll));
+        board.set_piece_at(c.down_left().unwrap(), Some(Piece::Troll));
+        board.set_piece_at(c.down_right().unwrap(), Some(Piece::Troll));
         board
     }
 
@@ -174,4 +189,291 @@ impl BoardState {
         let Coordinate(offset) = c;
         self.spaces[offset as usize] = p;
     }
+}
+
+#[derive(Clone, Copy, Eq, PartialEq)]
+pub enum Player {
+    Dwarf,
+    Troll,
+}
+
+pub struct GameState {
+    current_player: Player,
+    actions: Vec<Action>,
+    board: BoardState,
+}
+
+macro_rules! build_dwarf_move_actions {
+    ($board: expr, $start: expr, $mutator_expr: expr, $result: ident) => (
+        {
+            let mutator = $mutator_expr;
+            let mut end = mutator($start);
+            loop {
+                match end {
+                    Some(e) if $board.piece_at(e).is_none() => {
+                        $result.push(Action::Move($start, e));
+                        end = mutator(e);
+                    },
+                    _ => break,
+                }
+            }
+        })
+}
+
+macro_rules! build_dwarf_hurl_actions {
+    ($board: expr, $start: expr, $forward_expr: expr, $backward_expr: expr, $result: ident) => (
+        {
+            let forward = $forward_expr;
+            let backward = $backward_expr;
+            let mut end = forward($start);
+            let mut line_start = $start;
+            loop {
+                match end {
+                    Some(e) if $board.piece_at(e) == Some(Piece::Troll) => {  // Land on a troll.
+                        $result.push(Action::Hurl($start, e));
+                        break
+                    },
+                    Some(e) if $board.piece_at(e).is_none() => {  // No obstacles.
+                        match backward(line_start) {
+                            Some(s) if $board.piece_at(s) == Some(Piece::Dwarf) => {
+                                // More dwarfs behind, so continue search.
+                                line_start = s;
+                                end = forward(e);
+                            },
+                            _ => break,  // No more dwarfs behind.
+                        }
+                    },
+                    _ => break,  // Ran off of end of board or hit a dwarf or the Thudstone.
+                }
+            }
+        })
+}
+
+macro_rules! build_troll_move_actions {
+    ($board: expr, $start: expr, $mutator_expr: expr, $result: ident) => (
+        {
+            let mutator = $mutator_expr;
+            match mutator($start) {
+                Some(e) if $board.piece_at(e).is_none() =>
+                    $result.push(Action::Move($start, e)),  // Nothing in the way.
+                _ => (),  // Obstacle.
+            }
+        })
+}
+
+macro_rules! build_troll_shove_actions {
+    ($board: expr, $start: expr, $forward_expr: expr, $backward_expr: expr, $result: ident) => (
+        {
+            let forward = $forward_expr;
+            let backward = $backward_expr;
+            let mut end = forward($start);
+            let mut line_start = $start;
+            loop {
+                match end {
+                    Some(e) if $board.piece_at(e) == None => {
+                        if (e.up().and_then(|c| $board.piece_at(c))
+                            .or_else(|| e.down().and_then(|c| $board.piece_at(c)))
+                            .or_else(|| e.left().and_then(|c| $board.piece_at(c)))
+                            .or_else(|| e.right().and_then(|c| $board.piece_at(c)))
+                            .or_else(|| e.up_left().and_then(|c| $board.piece_at(c)))
+                            .or_else(|| e.up_right().and_then(|c| $board.piece_at(c)))
+                            .or_else(|| e.down_left().and_then(|c| $board.piece_at(c)))
+                            .or_else(|| e.down_right().and_then(|c| $board.piece_at(c))))
+                            == Some(Piece::Dwarf) {
+                                // At least one dwarf is adjacent to this space, so a
+                                // shove that lands here will capture.
+                                $result.push(Action::Shove($start, e));
+                                match backward(line_start) {
+                                    Some(s) if $board.piece_at(s) == Some(Piece::Troll) => {
+                                        // More trolls behind, so continue search.
+                                        line_start = s;
+                                        end = forward(e);
+                                    },
+                                    _ => break,  // No more trolls behind.
+                                }
+                            }
+                    },
+                    _ => break,  // Ran off of end of board or hit an occupied square.
+                }
+            }
+        })
+}
+
+impl GameState {
+    pub fn new() -> Self {
+        GameState {
+            current_player: Player::Dwarf,
+            actions: vec![],
+            board: BoardState::new(),
+        }
+    }
+
+    pub fn player_actions(&self, p: Player) -> Vec<Action> {
+        let mut result = vec![];
+        match p {
+            Player::Dwarf =>
+                for i in 0u8..165u8 {
+                    let start = Coordinate::new(i);
+                    if let Some(Piece::Dwarf) = self.board.piece_at(start) {
+                        // Move.
+                        build_dwarf_move_actions![
+                            self.board, start, |c: Coordinate| c.up(), result];
+                        build_dwarf_move_actions![
+                            self.board, start, |c: Coordinate| c.down(), result];
+                        build_dwarf_move_actions![
+                            self.board, start, |c: Coordinate| c.left(), result];
+                        build_dwarf_move_actions![
+                            self.board, start, |c: Coordinate| c.right(), result];
+                        build_dwarf_move_actions![
+                            self.board, start,
+                            |c: Coordinate| c.up_left(), result];
+                        build_dwarf_move_actions![
+                            self.board, start,
+                            |c: Coordinate| c.up_right(), result];
+                        build_dwarf_move_actions![
+                            self.board, start,
+                            |c: Coordinate| c.down_left(), result];
+                        build_dwarf_move_actions![
+                            self.board, start,
+                            |c: Coordinate| c.down_right(), result];
+                        // Hurl.
+                        build_dwarf_hurl_actions![
+                            self.board, start,
+                            |c: Coordinate| c.up(), |c: Coordinate| c.down(), result];
+                        build_dwarf_hurl_actions![
+                            self.board, start,
+                            |c: Coordinate| c.down(), |c: Coordinate| c.up(), result];
+                        build_dwarf_hurl_actions![
+                            self.board, start,
+                            |c: Coordinate| c.left(), |c: Coordinate| c.right(), result];
+                        build_dwarf_hurl_actions![
+                            self.board, start,
+                            |c: Coordinate| c.right(), |c: Coordinate| c.left(), result];
+                        build_dwarf_hurl_actions![
+                            self.board, start,
+                            |c: Coordinate| c.up_left(), |c: Coordinate| c.down_right(), result];
+                        build_dwarf_hurl_actions![
+                            self.board, start,
+                            |c: Coordinate| c.up_right(), |c: Coordinate| c.down_left(), result];
+                        build_dwarf_hurl_actions![
+                            self.board, start,
+                            |c: Coordinate| c.down_left(), |c: Coordinate| c.up_right(), result];
+                        build_dwarf_hurl_actions![
+                            self.board, start,
+                            |c: Coordinate| c.down_right(), |c: Coordinate| c.up_left(), result];
+                    }
+                },
+            Player::Troll =>
+                for i in 0u8..165u8 {
+                    let start = Coordinate::new(i);
+                    if let Some(Piece::Troll) = self.board.piece_at(start) {
+                        // Move.
+                        build_troll_move_actions![
+                            self.board, start, |c: Coordinate| c.up(), result];
+                        build_troll_move_actions![
+                            self.board, start, |c: Coordinate| c.down(), result];
+                        build_troll_move_actions![
+                            self.board, start, |c: Coordinate| c.left(), result];
+                        build_troll_move_actions![
+                            self.board, start, |c: Coordinate| c.right(), result];
+                        build_troll_move_actions![
+                            self.board, start,
+                            |c: Coordinate| c.up_left(), result];
+                        build_troll_move_actions![
+                            self.board, start,
+                            |c: Coordinate| c.up_right(), result];
+                        build_troll_move_actions![
+                            self.board, start,
+                            |c: Coordinate| c.down_left(), result];
+                        build_troll_move_actions![
+                            self.board, start,
+                            |c: Coordinate| c.down_right(), result];
+                        // Shove.
+                        build_troll_shove_actions![
+                            self.board, start,
+                            |c: Coordinate| c.up(), |c: Coordinate| c.down(), result];
+                        build_troll_shove_actions![
+                            self.board, start,
+                            |c: Coordinate| c.down(), |c: Coordinate| c.up(), result];
+                        build_troll_shove_actions![
+                            self.board, start,
+                            |c: Coordinate| c.left(), |c: Coordinate| c.right(), result];
+                        build_troll_shove_actions![
+                            self.board, start,
+                            |c: Coordinate| c.right(), |c: Coordinate| c.left(), result];
+                        build_troll_shove_actions![
+                            self.board, start,
+                            |c: Coordinate| c.up_left(),
+                            |c: Coordinate| c.down_right(), result];
+                        build_troll_shove_actions![
+                            self.board, start,
+                            |c: Coordinate| c.up_right(),
+                            |c: Coordinate| c.down_left(), result];
+                        build_troll_shove_actions![
+                            self.board, start,
+                            |c: Coordinate| c.down_left(),
+                            |c: Coordinate| c.up_right(), result];
+                        build_troll_shove_actions![
+                            self.board, start,
+                            |c: Coordinate| c.down_right(),
+                            |c: Coordinate| c.up_left(), result];
+                    }
+                },
+            }
+        result
+    }
+
+    pub fn player(&self) -> Player {
+        self.current_player
+    }
+
+    pub fn advance_player(&mut self) {
+        self.current_player = match self.current_player {
+            Player::Dwarf => Player::Troll,
+            Player::Troll => Player::Dwarf,
+        }
+    }
+
+    fn remove_adjacent_dwarves(&mut self, end: Coordinate) {
+        for adjacency in [end.up(), end.down(), end.left(), end.right(),
+                          end.up_left(), end.up_right(),
+                          end.down_left(), end.down_right()].iter() {
+            if let Some((e, Some(Piece::Dwarf))) = adjacency.map(|c| (c, self.board.piece_at(c))) {
+                self.board.set_piece_at(e, None);
+            }
+        }
+    }
+
+    pub fn take_action(&mut self, a: Action) {
+        // TODO: validation.
+        self.actions.push(a);
+        match a {
+            Action::Move(start, end) => {
+                let p = self.board.piece_at(start);
+                self.board.set_piece_at(end, p);
+                self.board.set_piece_at(start, None);
+                if self.current_player == Player::Troll {
+                    self.remove_adjacent_dwarves(end);
+                }
+            },
+            Action::Hurl(start, end) => {
+                let p = self.board.piece_at(start);
+                self.board.set_piece_at(end, p);
+                self.board.set_piece_at(start, None);
+            },
+            Action::Shove(start, end) => {
+                let p = self.board.piece_at(start);
+                self.board.set_piece_at(end, p);
+                self.board.set_piece_at(start, None);
+                self.remove_adjacent_dwarves(end);
+            },
+        }
+    }
+}
+
+#[derive(Clone, Copy, Eq, PartialEq)]
+pub enum Action {
+    Move(Coordinate, Coordinate),
+    Hurl(Coordinate, Coordinate),
+    Shove(Coordinate, Coordinate),
 }
