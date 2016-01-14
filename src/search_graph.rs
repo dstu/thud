50e1ab37b5989c3dc53b0df6a010e6e473be99f9
Stu Black <trurl@freeshell.org>
2016-01-14 00:44:47 -0500

    Wrote some documentation for the search_graph module.

--- src/search_graph.rs
+++ src/search_graph.rs
@@ -6,43 +6,68 @@ use std::clone::Clone;
 use std::fmt::Debug;
 use std::hash::Hash;
 
+/// Internal derived type for edge IDs.
+///
+/// This type is not exposed publicly because it does not identify the graph
+/// that it belongs to, which makes it only slightly less dangerous than a
+/// pointer with no lifetime.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 struct ArcId(usize);
 
 impl ArcId {
+    /// Converts an `ArcId` to a usize to index into a collection.
     fn as_usize(self) -> usize {
         let ArcId(value) = self;
         value
     }
 }
 
+/// Internal derived type for vertex IDs.
+///
+/// For a given graph, distinct `StateId`s are associated with distinct game
+/// states. This type is not exposed publicly because it does not identify the
+/// graph that it belongs to, which makes it only slightly less dangerous than a
+/// pointer with no lifetime.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 struct StateId(usize);
 
 impl StateId {
+    /// Converts a `StateId` to a usize to index into a collection.
     fn as_usize(self) -> usize {
         let StateId(value) = self;
         value
     }
 }
 
+/// Retains a mapping from game states to distinct IDs.
+///
+/// The game state type `T` is required to derive from `Clone` to accommodate a
+/// limitation of the `HashMap` interface.
 struct StateNamespace<T> where T: Hash + Eq + Clone {
     states: HashMap<T, StateId>,
 }
 
+/// The result of inserting a game state into a `StateNamespace`.
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 enum NamespaceInsertion {
+    /// State was already present, with the given ID.
     Present(StateId),
+    /// State was new, and is inserted with the given ID.
     New(StateId),
 }
 
 impl<T> StateNamespace<T> where T: Hash + Eq + Clone {
+    /// Creates a new, empty `StateNamespace`.
     fn new() -> Self {
         StateNamespace {
             states: HashMap::new(),
         }
     }
 
+    /// Retrieves a `StateId` for `state`, creating a new one if necessary.
+    ///
+    /// This may insert a new state into `self` or simply retrieve the `StateId`
+    /// associated with it in a prior insertion operation.
     fn get_or_insert(&mut self, state: T) -> NamespaceInsertion {
         let next_state_id = StateId(self.states.len());
         match self.states.entry(state) {
@@ -51,14 +76,31 @@ impl<T> StateNamespace<T> where T: Hash + Eq + Clone {
         }
     }
 
+    /// Retrieves a `StateId` for `state`.
+    ///
+    /// If `state` has not been inserted, returns `None`.
     fn get(&self, state: &T) -> Option<StateId> {
         self.states.get(state).map(|x| *x)
     }
 }
 
+/// The target of an outgoing graph edge.
+///
+/// A search graph is built up incrementally. Any vertices in its initial state
+/// are typically added with all of their edges in the unexpanded
+/// state. Graph-modifying operations which are executed while exploring the
+/// game state topology will expand these edges. Cycle detection is done at edge
+/// expansion time.
 pub enum Target<T, R> {
+    /// Edge has not yet been expanded.
     Unexpanded(R),
+    /// Edge has been expanded but leads to a cycle. Because cycle detection is
+    /// done at edge expansion time, this usually means that another edge, which
+    /// was expanded previously, has the value `Target::Expanded` and points to
+    /// the same vertex.
     Cycle(T),
+    /// Edge has been expanded and was the expanded edge that lead to the game
+    /// state which it points to.
     Expanded(T),
 }
 
@@ -84,27 +126,50 @@ impl<T, R> fmt::Debug for Target<T, R> where T: fmt::Debug, R: fmt::Debug {
     }
 }
 
+/// Internal type for graph vertices.
 #[derive(Debug)]
 struct Vertex<S> where S: Debug {
+    /// Vertex data.
     data: S,
+    /// Parent edges pointing into this vertex.
     parents: Vec<ArcId>,
+    /// Child edges pointing out of this vertex.
     children: Vec<ArcId>,
 }
 
+/// Internal type for graph edges.
 #[derive(Debug)]
 struct Arc<A> where A: Debug {
+    /// Edge data.
     data: A,
+    /// Source vertex.
     source: StateId,
+    /// Target vertex. If this arc is unexpanded, it is
+    /// `Target::Unexpanded(())`; otherwise, it is either `Target::Cycle(id)` or
+    /// `Target::Expanded(id)` for target vertex with a `StateId` of `id`.
     target: Target<StateId, ()>,
 }
 
+/// A search graph.
+///
+/// Supports incremental rollout of game state topology, vertex de-duplication
+/// with transposition tables, and cycle detection. Does not support deletion.
+///
+/// - `T`: The type of game states. It is required to derive `Hash` and `Eq` to
+///   so that it may be stored in a hashtable, where game states are looked up to
+///   support de-duplication of game states. It is required to derive `Clone` to
+///   accommodate a limitation of the `HashMap` interface.
+/// - `S`: The type of graph vertex data.
+/// - `A`: The type of graph edge data.
 pub struct Graph<T, S, A> where T: Hash + Eq + Clone, S: Debug, A: Debug {
+    /// Lookup table that maps from game states to `StateId`.
     state_ids: StateNamespace<T>,
     vertices: Vec<Vertex<S>>,  // Indexed by StateId.
     arcs: Vec<Arc<A>>,  // Indexed by ArcId.
 }
 
 impl<T, S, A> Graph<T, S, A> where T: Hash + Eq + Clone, S: Debug, A: Debug {
+    /// Creates an empty `Graph` with no vertices or edges.
     pub fn new() -> Self {
         Graph {
             state_ids: StateNamespace::new(),
@@ -113,27 +178,40 @@ impl<T, S, A> Graph<T, S, A> where T: Hash + Eq + Clone, S: Debug, A: Debug {
         }
     }
 
+    /// Returns the vertex for the given `StateId`.
     fn get_vertex(&self, state: StateId) -> &Vertex<S> {
         &self.vertices[state.as_usize()]
     }
 
+    /// Returns the vertex for the given `StateId`.
     fn get_vertex_mut(&mut self, state: StateId) -> &mut Vertex<S> {
         &mut self.vertices[state.as_usize()]
     }
 
+    /// Returns the edge for the given `ArcId`.
     fn get_arc(&self, arc: ArcId) -> &Arc<A> {
         &self.arcs[arc.as_usize()]
     }
 
+    /// Returns the edge for the given `ArcId`.
     fn get_arc_mut(&mut self, arc: ArcId) -> &mut Arc<A> {
         &mut self.arcs[arc.as_usize()]
     }
 
+    /// Adds a new vertex with the given data, returning a mutable reference to it.
+    ///
+    /// This method does not add incoming or outgoing edges (expanded or
+    /// not). That must be done by calling `add_arc` with the new vertex
+    /// `StateId`.
     fn add_vertex(&mut self, data: S) -> &mut Vertex<S> {
         self.vertices.push(Vertex { data: data, parents: Vec::new(), children: Vec::new() });
         self.vertices.last_mut().unwrap()
     }
 
+    /// Adds a new edge with the given data, source, and target.
+    ///
+    /// Iff `target` is `Target::Expanded(id)`, the vertex with `StateId` of
+    /// `id` will have the vertex `source` added as a parent.
     fn add_arc(&mut self, data: A, source: StateId, target: Target<StateId, ()>) {
         let arc = Arc { data: data, source: source, target: target, };
         let arc_id = ArcId(self.arcs.len());
@@ -144,6 +222,11 @@ impl<T, S, A> Graph<T, S, A> where T: Hash + Eq + Clone, S: Debug, A: Debug {
         self.arcs.push(arc);
     }
 
+    /// Checks whether a path exists from `source` to `target`.
+    ///
+    /// Paths are found using a simple depth-first search. This method only
+    /// follows arcs with destination type `Target::Expanded`, so it does not
+    /// find paths that go through an ancestor of `source`.
     fn path_exists(&self, source: StateId, target: StateId) -> bool {
         let mut frontier = vec![source];
         while !frontier.is_empty() {
@@ -161,6 +244,9 @@ impl<T, S, A> Graph<T, S, A> where T: Hash + Eq + Clone, S: Debug, A: Debug {
         false
     }
 
+    /// Gets a node handle for the given game state.
+    ///
+    /// If `state` does not correspond to a known game state, returns `None`.
     pub fn get_node<'s>(&'s self, state: &T) -> Option<Node<'s, T, S, A>> {
         match self.state_ids.get(&state) {
             Some(id) => Some(Node { graph: self, id: id, }),
@@ -168,6 +254,9 @@ impl<T, S, A> Graph<T, S, A> where T: Hash + Eq + Clone, S: Debug, A: Debug {
         }
     }
 
+    /// Gets a mutable node handle for the given game state.
+    ///
+    /// If `state` does not correspond to a known game state, returns `None`.
     pub fn get_node_mut<'s>(&'s mut self, state: &T) -> Option<MutNode<'s, T, S, A>> {
         match self.state_ids.get(state) {
             Some(id) => Some(MutNode { graph: self, id: id, }),
@@ -175,6 +264,13 @@ impl<T, S, A> Graph<T, S, A> where T: Hash + Eq + Clone, S: Debug, A: Debug {
         }
     }
 
+    /// Adds a root vertex (one with no parents) for the given game state and
+    /// data and returns a mutable handle for it.
+    ///
+    /// If `state` is already known, returns a mutable handle to that state,
+    /// ignoring the `data` parameter. As a result, this method is guaranteed to
+    /// return a handle for a root vertex only when `state` is a novel game
+    /// state.
     pub fn add_root<'s>(&'s mut self, state: T, data: S) -> MutNode<'s, T, S, A> {
         let node_id = match self.state_ids.get_or_insert(state) {
             NamespaceInsertion::Present(id) => id,
@@ -187,6 +283,10 @@ impl<T, S, A> Graph<T, S, A> where T: Hash + Eq + Clone, S: Debug, A: Debug {
     }
 }
 
+/// Immutable handle to a graph vertex, called a "node handle."
+///
+/// This zipper-like type enables traversal of a graph along the vertex's
+/// incoming and outgoing edges.
 pub struct Node<'a, T, S, A> where T: Hash + Eq + Clone + 'a, S: Debug + 'a, A: Debug + 'a {
     graph: &'a Graph<T, S, A>,
     id: StateId,
@@ -197,6 +297,8 @@ impl<'a, T, S, A> Node<'a, T, S, A> where T: Hash + Eq + Clone + 'a, S: Debug + 
         &self.graph.get_vertex(self.id).children
     }
 
+    /// Returns an immutable ID that is guaranteed to identify this vertex
+    /// uniquely within its graph.
     pub fn get_id(&self) -> usize {
         self.id.as_usize()
     }
@@ -209,23 +311,29 @@ impl<'a, T, S, A> Node<'a, T, S, A> where T: Hash + Eq + Clone + 'a, S: Debug + 
         &self.graph.get_vertex(self.id).data
     }
 
+    /// Returns true iff this vertex has no outgoing edges (regardless of
+    /// whether they are expanded).
     pub fn is_leaf(&self) -> bool {
         self.children().is_empty()
     }
 
+    /// Returns true iff this vertex has no incoming edges.
     pub fn is_root(&self) -> bool {
         self.parents().is_empty()
     }
 
+    /// Returns a traversible list of outgoing edges.
     pub fn get_child_list(&self) -> ChildList<'a, T, S, A> {
         ChildList { graph: self.graph, id: self.id, }
     }
 
+    /// Returns a traversible list of incoming edges.
     pub fn get_parent_list(&self) -> ParentList<'a, T, S, A> {
         ParentList { graph: self.graph, id: self.id, }
     }
 }
 
+/// A traversible list of a vertex's outgoing edges.
 pub struct ChildList<'a, T, S, A> where T: Hash + Eq + Clone + 'a, S: Debug + 'a, A: Debug + 'a {
     graph: &'a Graph<T, S, A>,
     id: StateId,
@@ -235,24 +343,29 @@ impl<'a, T, S, A> ChildList<'a, T, S, A> where T: Hash + Eq + Clone + 'a, S: Deb
     fn vertex(&self) -> &'a Vertex<S> {
         self.graph.get_vertex(self.id)
     }
-    
+
+    /// Returns the number of edges.
     pub fn len(&self) -> usize {
         self.vertex().children.len()
     }
 
+    /// Returns true iff there are no outgoing edges.
     pub fn is_empty(&self) -> bool {
         self.vertex().children.is_empty()
     }
 
+    /// Returns a node handle for the vertex these edges originate from.
     pub fn get_source_node(&self) -> Node<'a, T, S, A> {
         Node { graph: self.graph, id: self.id, }
     }
 
+    /// Returns an edge handle for the `i`th edge.
     pub fn get_edge(&self, i: usize) -> Edge<'a, T, S, A> {
         Edge { graph: self.graph, id: self.vertex().children[i], }
     }
 }
 
+/// A traversible list of a vertex's incoming edges.
 pub struct ParentList<'a, T, S, A> where T: Hash + Eq + Clone + 'a, S: Debug + 'a, A: Debug + 'a {
     graph: &'a Graph<T, S, A>,
     id: StateId,
@@ -263,23 +376,31 @@ impl<'a, T, S, A> ParentList<'a, T, S, A> where T: Hash + Eq + Clone + 'a, S: De
         self.graph.get_vertex(self.id)
     }
 
+    /// Returns the number of edges.
     pub fn len(&self) -> usize {
         self.vertex().parents.len()
     }
 
+    /// Returns true iff there are no incoming edges.
     pub fn is_empty(&self) -> bool {
         self.vertex().parents.is_empty()
     }
 
+    /// Returns a node handle for the vertex these edges point to.
     pub fn target_node(&self) -> Node<'a, T, S, A> {
         Node { graph: self.graph, id: self.id, }
     }
 
+    /// Returns an edge handle for the `i`th edge.
     pub fn get_edge(&self, i: usize) -> Edge<'a, T, S, A> {
         Edge { graph: self.graph, id: self.vertex().parents[i] }
     }
 }
 
+/// Immutable handle to a graph edge, called an "edge handle."
+///
+/// This zipper-like type enables traversal of a graph along the edge's source
+/// and target vertices.
 pub struct Edge<'a, T, S, A> where T: Hash + Eq + Clone + 'a, S: Debug + 'a, A: Debug + 'a {
     graph: &'a Graph<T, S, A>,
     id: ArcId,
@@ -290,6 +411,8 @@ impl<'a, T, S, A> Edge<'a, T, S, A> where T: Hash + Eq + Clone + 'a, S: Debug + 
         self.graph.get_arc(self.id)
     }
 
+    /// Returns an immutable ID that is guaranteed to identify this edge
+    /// uniquely within its graph.
     pub fn get_id(&self) -> usize {
         self.id.as_usize()
     }
@@ -298,10 +421,12 @@ impl<'a, T, S, A> Edge<'a, T, S, A> where T: Hash + Eq + Clone + 'a, S: Debug + 
         &self.arc().data
     }
 
+    /// Returns a node handle for this edge's source vertex.
     pub fn get_source(&self) -> Node<'a, T, S, A> {
         Node { graph: self.graph, id: self.arc().source, }
     }
 
+    /// Returns a node handle for this edge's target vertex.
     pub fn get_target(&self) -> Target<Node<'a, T, S, A>, ()> {
         match self.arc().target {
             Target::Cycle(id) => Target::Cycle(Node { graph: self.graph, id: id, }),
