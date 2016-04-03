db8f12cba57c8bc7e5ea621aa4d9eb2f1dcf1a0b
Stu Black <trurl@freeshell.org>
2016-04-02 22:08:18 -0400

    Add'l debug output for parent selection in backprop and remove erroneous comment.

--- src/mcts/backprop.rs
+++ src/mcts/backprop.rs
@@ -35,7 +35,7 @@ impl<'a> Iterator for ParentSelectionIter<'a> {
                 None => return None,
                 Some((i, ref e)) => {
                     if ucb::is_best_child(e, self.role, self.explore_bias) {
-                        trace!("ParentSelectionIter::next: edge {} is a best child", e.get_id());
+                        trace!("ParentSelectionIter::next: edge {} (from node {}) is a best child", e.get_id(), e.get_source().get_id());
                         return Some(i)
                     }
                     trace!("ParentSelectionIter::next: edge {} (data {:?}) is not a best child", e.get_id(), e.get_data());
@@ -52,12 +52,6 @@ pub fn backprop_payoff<'a, R: Rng>(node: Node<'a>, epoch: usize, payoff: Payoff,
         let (player, node) = to_visit.pop().unwrap();
         trace!("backprop_payoff: looking at moves by player {:?} incoming to node {}", player, node.get_id());
         if node.get_data().visited_in_backprop_epoch(epoch) {
-            // NOTE: in the case that two child edges of a single node are both
-            // best children (e.g., when dealing with ties) and backprop would
-            // have us traverse both of them to the common ancestor, we
-            // currently choose to update only one of them. This effectively
-            // breaks symmetry between the two edges. Whether this is necessary
-            // or desirable remains to be seen.
             trace!("backprop_payoff: node {} already visited in epoch {}", node.get_id(), epoch);
         } else {
             let parents = node.get_parent_list();
