79261dd6e2a3fb918657a76fad0979b28c45daea
Stu Black <trurl@freeshell.org>
2019-01-09 23:38:06 -0500

    Defining a registry for selecting agents via the command line.
    thud_ui_common builds but has had a lot of stuff commented out.

--- thud_ui_common/src/ai/mcts.rs
+++ thud_ui_common/src/ai/mcts.rs
@@ -4,12 +4,57 @@ use std::marker::Send;
 use std::sync::mpsc;
 use std::sync::{Arc, Mutex};
 use std::thread;
-use {ThudGame, ThudState};
+
+/*
+pub const FLAG_ITERATION_COUNT: &'static str = "iterations";
+pub const FLAG_SIMULATION_COUNT: &'static str = "simulations";
+pub const FLAG_EXPLORATION_BIAS: &'static str = "explore_bias";
+pub const FLAG_AI_PLAYER: &'static str = "ai_player";
+pub const FLAG_MOVE_SELECTION_CRITERION: &'static str = "move_selection_criterion";
+pub const FLAG_RNG_SEED: &'static str = "rng_seed";
+pub const FLAG_COMPACT_SEARCH_GRAPH: &'static str = "compact_search_graph";
+
+      x if x == FLAG_ITERATION_COUNT => clap::Arg::with_name(ITERATION_COUNT_FLAG)
+        .short("i")
+        .long("iterations")
+        .takes_value(true)
+        .value_name("ITERATIONS")
+        .help("Number of Monte Carlo iterations to run per epoch")
+        .required(true),
+      x if x == FLAG_SIMULATION_COUNT => clap::Arg::with_name(SIMULATION_COUNT_FLAG)
+        .short("s")
+        .long("simulations")
+        .takes_value(true)
+        .value_name("SIMULATIONS")
+        .help("Number of simulations to run at each expansion step")
+        .required(true),
+      x if x == FLAG_EXPLORATION_BIAS => clap::Arg::with_name(EXPLORATION_BIAS_FLAG)
+        .short("b")
+        .long("exploration_bias")
+        .takes_value(true)
+        .value_name("BIAS")
+        .help("Exploration bias for UCB computation")
+        .required(true),
+      x if x == FLAG_MOVE_SELECTION_CRITERION => clap::Arg::with_name(MOVE_SELECTION_CRITERION_FLAG)
+        .long("move_selection")
+        .takes_value(true)
+        .possible_values(&["visitcount", "ucb"])
+        .help("Criteria for selecting the best move to make"),
+      x if x == FLAG_RNG_SEED => clap::Arg::with_name(RNG_SEED_FLAG)
+        .long("seed")
+        .takes_value(true)
+        .help("Manually specify RNG seed"),
+      x if x == FLAG_COMPACT_SEARCH_GRAPH => clap::Arg::with_name(COMPACT_SEARCH_GRAPH_FLAG)
+        .long("compact_graph")
+        .takes_value(false)
+        .help("Compact the search graph after each move"),
+
+*/
 
 pub enum Instruction {
   Exit,
   Search {
-    state: ThudState,
+    state: crate::State,
     iteration_count: u32,
     simulation_count: usize,
   },
@@ -19,7 +64,7 @@ pub struct Handle {
   status: Arc<Mutex<Status>>,
   join_handle: thread::JoinHandle<()>,
   parameters_tx: mpsc::SyncSender<Instruction>,
-  result_rx: mpsc::Receiver<Vec<mcts::ActionStatistics<ThudGame>>>,
+  result_rx: mpsc::Receiver<Vec<mcts::ActionStatistics<crate::Game>>>,
 }
 
 #[derive(Clone)]
@@ -31,7 +76,7 @@ pub enum Status {
   /// Computing search.
   Working {
     /// Statistics returned from last search iteration.
-    statistics: Vec<mcts::ActionStatistics<ThudGame>>,
+    statistics: Vec<mcts::ActionStatistics<crate::Game>>,
     /// Iteration being computed (0-based).
     iteration: u32,
     /// Total iterations to complete.
@@ -54,8 +99,8 @@ impl Handle {
     let join_handle = {
       let status = status.clone();
       thread::spawn(move || {
-        let mut graph = mcts::new_search_graph::<ThudGame>();
-        let mut search_state = mcts::SearchState::<R, ThudGame>::new(rng, exploration_bias);
+        let mut graph = mcts::new_search_graph::<crate::Game>();
+        let mut search_state = mcts::SearchState::<R, crate::Game>::new(rng, exploration_bias);
         // Read search parameters (blocks until an instruction is provided).
         while let Instruction::Search {
           state,
--- thud_ui_common/src/lib.rs
+++ thud_ui_common/src/lib.rs
@@ -1,42 +1,27 @@
-extern crate chrono;
-#[macro_use]
-extern crate clap;
-#[macro_use]
-extern crate log;
-extern crate fern;
-extern crate mcts;
-extern crate rand;
-extern crate thud_game;
+use clap::{self, arg_enum, _clap_count_exprs};
+use thud_game::{self, board};
 
-use clap::{App, Arg};
-use thud_game::board;
+// pub use thud_game::ai::mcts::deconvolve_transpositions::Game as ThudGame;
+// pub use thud_game::ai::mcts::deconvolve_transpositions::Payoff as ThudPayoff;
+// pub use thud_game::ai::mcts::deconvolve_transpositions::State as ThudState;
+// pub use thud_game::ai::mcts::deconvolve_transpositions::Statistics as ThudStatistics;
 
-pub use thud_game::ai::mcts::deconvolve_transpositions::Game as ThudGame;
-pub use thud_game::ai::mcts::deconvolve_transpositions::Payoff as ThudPayoff;
-pub use thud_game::ai::mcts::deconvolve_transpositions::State as ThudState;
-pub use thud_game::ai::mcts::deconvolve_transpositions::Statistics as ThudStatistics;
+pub mod agent_registry;
+// pub mod init;
+// pub mod ai;
 
-pub mod ai;
-
-pub const ITERATION_COUNT_FLAG: &'static str = "iterations";
-pub const SIMULATION_COUNT_FLAG: &'static str = "simulations";
-pub const EXPLORATION_BIAS_FLAG: &'static str = "explore_bias";
-pub const INITIAL_BOARD_FLAG: &'static str = "initial_board";
-pub const INITIAL_PLAYER_FLAG: &'static str = "initial_player";
-pub const AI_PLAYER_FLAG: &'static str = "ai_player";
-pub const LOG_LEVEL_FLAG: &'static str = "log_level";
-pub const MOVE_SELECTION_CRITERION_FLAG: &'static str = "move_selection_criterion";
-pub const RNG_SEED_FLAG: &'static str = "rng_seed";
-pub const COMPACT_SEARCH_GRAPH_FLAG: &'static str = "compact_search_graph";
+pub const FLAG_INITIAL_BOARD: &'static str = "initial_board";
+pub const FLAG_INITIAL_PLAYER: &'static str = "initial_player";
+pub const FLAG_LOG_LEVEL: &'static str = "log_level";
 
 arg_enum! {
-    #[derive(Debug)]
-    pub enum InitialBoard {
-        Default,
-        TrollEndgame,
-        DwarfEndgame,
-        DwarfBoxed
-    }
+  #[derive(Debug)]
+  pub enum InitialBoard {
+    Default,
+    TrollEndgame,
+    DwarfEndgame,
+    DwarfBoxed
+  }
 }
 
 impl InitialBoard {
@@ -51,11 +36,11 @@ impl InitialBoard {
 }
 
 arg_enum! {
-    #[derive(Debug, Clone, Copy)]
-    pub enum MoveSelectionCriterion {
-        VisitCount,
-        Ucb
-    }
+  #[derive(Debug, Clone, Copy)]
+  pub enum MoveSelectionCriterion {
+    VisitCount,
+    Ucb
+  }
 }
 
 const DEFAULT_CELLS: &'static str = r#"
@@ -130,87 +115,33 @@ _dd_________dd_
 .....d___d.....
 "#;
 
-pub fn set_args<'a, 'b>(app: App<'a, 'b>, flags: &[&str]) -> App<'a, 'b>
+/// Adds arguments to `app` that are common across all user-facing UIs. These
+/// arguments come from constants in this module. Returns the updated `App`.
+pub fn set_args<'a, 'b>(app: clap::App<'a, 'b>, flags: &[&str]) -> clap::App<'a, 'b>
 where
   'a: 'b,
 {
-  let populated_flags: Vec<Arg<'static, 'static>> = flags
+  let populated_flags: Vec<clap::Arg<'static, 'static>> = flags
     .iter()
     .map(|f| match *f {
-      x if x == ITERATION_COUNT_FLAG => Arg::with_name(ITERATION_COUNT_FLAG)
-        .short("i")
-        .long("iterations")
-        .takes_value(true)
-        .value_name("ITERATIONS")
-        .help("Number of Monte Carlo iterations to run per epoch")
-        .required(true),
-      x if x == SIMULATION_COUNT_FLAG => Arg::with_name(SIMULATION_COUNT_FLAG)
-        .short("s")
-        .long("simulations")
-        .takes_value(true)
-        .value_name("SIMULATIONS")
-        .help("Number of simulations to run at each expansion step")
-        .required(true),
-      x if x == EXPLORATION_BIAS_FLAG => Arg::with_name(EXPLORATION_BIAS_FLAG)
-        .short("b")
-        .long("exploration_bias")
-        .takes_value(true)
-        .value_name("BIAS")
-        .help("Exploration bias for UCB computation")
-        .required(true),
-      x if x == INITIAL_BOARD_FLAG => Arg::with_name(INITIAL_BOARD_FLAG)
+      x if x == FLAG_INITIAL_BOARD => clap::Arg::with_name(FLAG_INITIAL_BOARD)
         .long("board")
         .takes_value(true)
         .possible_values(&["default", "trollendgame", "dwarfendgame", "dwarfboxed"])
         .help("Initial board configuration"),
-      x if x == INITIAL_PLAYER_FLAG => Arg::with_name(INITIAL_PLAYER_FLAG)
+      x if x == FLAG_INITIAL_PLAYER => clap::Arg::with_name(FLAG_INITIAL_PLAYER)
         .short("p")
         .long("player")
         .takes_value(true)
         .possible_values(&["dwarf", "troll"])
         .help("Initial player to play"),
-      x if x == AI_PLAYER_FLAG => Arg::with_name(AI_PLAYER_FLAG)
-        .short("p")
-        .long("player")
-        .takes_value(true)
-        .possible_values(&["dwarf", "troll"])
-        .help("Side that the AI will play"),
-      x if x == LOG_LEVEL_FLAG => Arg::with_name(LOG_LEVEL_FLAG)
+      x if x == FLAG_LOG_LEVEL => clap::Arg::with_name(FLAG_LOG_LEVEL)
         .long("log_level")
         .takes_value(true)
         .possible_values(&["info", "trace", "error", "debug", "off"])
         .help("Logging level"),
-      x if x == MOVE_SELECTION_CRITERION_FLAG => Arg::with_name(MOVE_SELECTION_CRITERION_FLAG)
-        .long("move_selection")
-        .takes_value(true)
-        .possible_values(&["visitcount", "ucb"])
-        .help("Criteria for selecting the best move to make"),
-      x if x == RNG_SEED_FLAG => Arg::with_name(RNG_SEED_FLAG)
-        .long("seed")
-        .takes_value(true)
-        .help("Manually specify RNG seed"),
-      x if x == COMPACT_SEARCH_GRAPH_FLAG => Arg::with_name(COMPACT_SEARCH_GRAPH_FLAG)
-        .long("compact_graph")
-        .takes_value(false)
-        .help("Compact the search graph after each move"),
       x => panic!("Unrecognized flag identifier '{}'", x),
     })
     .collect();
   app.args(&populated_flags)
-}
-
-pub fn init_logger(logging_level: log::LogLevelFilter) {
-  let config = fern::DispatchConfig {
-    format: Box::new(
-      |msg: &str, level: &log::LogLevel, _location: &log::LogLocation| {
-        let time = chrono::Local::now().format("%Y-%m-%d %T%.3f%z").to_string();
-        format!("[{}][{}] {}", time, level, msg)
-      },
-    ),
-    output: vec![fern::OutputConfig::stdout()],
-    level: logging_level,
-  };
-  if let Err(e) = fern::init_global_logger(config, log::LogLevelFilter::Trace) {
-    panic!("Filed to initialize global logger: {}", e);
-  }
 }
