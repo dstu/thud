b858119d5d217954a710aad31cdda25f545c520c
Stu Black <trurl@freeshell.org>
2019-08-02 12:05:22 -0400

    Document the end-of-game Decision enum.

--- thud_game/src/end.rs
+++ thud_game/src/end.rs
@@ -1,6 +1,14 @@
+//! Representation for the end status of a game of Thud.
+//!
+//! A Thud game traditionally ends when both players agree that it should
+//! end. This is implemented as a proposal/counter-proposal process.
+
+/// The decision made by a player to whom an end to the game has been proposed.
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub enum Decision {
+  /// Player accepts that the game should end.
   Accept,
+  /// Player does not accept that the game should end.
   Decline,
 }
 
